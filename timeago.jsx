89d545ecb240870ea1e746c12d55fc7bbc1eb82e
Joel Burget <joel@khanacademy.org>
2014-04-08 15:07:15 -0700

    Add TimeAgo.

---
+++ timeago.jsx
@@ -0,0 +1,33 @@
+/** @jsx React.DOM */
+
+/* The equivalent to jQuery.timeago for react.
+ *
+ * TimeAgo returns a span containing the amount of time (in English) that has
+ * passed since `time`.
+ *
+ * Takes:
+ *     time: an ISO 8601 timestamp
+ *     refreshMillis: how often to update, in milliseconds
+ *
+ * Example:
+ *
+ *     return <a href={khanAcademy><TimeAgo time={date} /></a>
+ */
+
+var SetIntervalMixin = require("./setinterval-mixin.js");
+// TODO(joel) - require moment
+
+// TODO(joel) i18n
+var TimeAgo = React.createClass({
+    mixins: [SetIntervalMixin],
+    render: function() {
+        return <span>{moment(this.props.time).fromNow()}</span>;
+    },
+    componentDidMount: function() {
+        var interval = this.props.time || 60000;
+        // TODO(joel) why did I have to bind forceUpdate?
+        this.setInterval(this.forceUpdate.bind(this), interval);
+    }
+});
+
+module.exports = TimeAgo;
