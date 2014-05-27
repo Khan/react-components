b44e2137444253549b97fa59fdca17abddfc16f4
Joel Burget <joel@khanacademy.org>
2014-05-27 13:56:14 -0700

    Add tooltip
    Auditors: jack

---
+++ examples/tooltip.jsx
@@ -0,0 +1,12 @@
+<Tooltip
+        className="class-for-tooltip-contents"
+        horizontalPosition="left"
+        horizontalAlign="left"
+        verticalPosition="bottom"
+        arrowSize={10}
+        borderColor="#ccc"
+        >
+    <TargetElementOfTheTooltip />
+    <TooltipContents1 />
+    <TooltipContents2 />
+</Tooltip>
