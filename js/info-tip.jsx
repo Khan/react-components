6218769e5232a53c614d4e55b1aba4c80e7c5ee2
Joel Burget <joel@khanacademy.org>
2014-05-28 15:46:22 -0700

    Switch to RCSS for styles!

--- js/info-tip.jsx
+++ js/info-tip.jsx
@@ -1,5 +1,81 @@
 /** @jsx React.DOM */
 
+var React = require('react');
+var RCSS = require('rcss');
+var _ = require('underscore');
+
+var infoTip = RCSS.createClass({
+    marginLeft: '5px',
+    position: 'relative'
+});
+
+var infoTipI = RCSS.createClass({
+    cursor: 'pointer'
+});
+
+var infoTipContainer = RCSS.createClass({
+    position: 'absolute',
+    'top': '-12px',
+    left: '22px',
+    zIndex: '1000'
+});
+
+var triangleBeforeAfter = {
+    borderBottom: '9px solid transparent',
+    borderTop: '9px solid transparent',
+    content: ' ',
+    display: 'block',
+    height: '0',
+    position: 'absolute',
+    'top': '0',
+    width: '0'
+};
+
+var infoTipTriangle = RCSS.createClass({
+    display: 'block',
+    height: '10px',
+    left: '0',
+    position: 'absolute',
+    'top': '8px',
+    width: '0',
+    zIndex: '1',
+
+    ':before': _.extend({}, triangleBeforeAfter, {
+        borderRight: '9px solid #bbb',
+        right: '0',
+    }),
+
+    ':after': _.extend({}, triangleBeforeAfter, {
+        borderRight: `9px solid ${colors.white}`,
+        right: '-1px'
+    })
+});
+
+var colors = {
+    grayLight: '#aaa',
+    basicBorderColor: '#ccc',
+    white: '#fff'
+};
+
+var basicBorder = RCSS.createClass({
+    border: `1px solid ${colors.basicBorderColor}`
+});
+
+var boxShadow = str => RCSS.createClass({ boxShadow: str })
+
+var verticalShadow = RCSS.merge(
+    basicBorder,
+    boxShadow(`0 1px 3px ${colors.basicBorderColor}`),
+    { borderBottom: `1px solid ${colors.grayLight}` }
+);
+
+var infoTipContentContainer = RCSS.merge(verticalShadow, {
+    display: 'block',
+    background: colors.white,
+    padding: '5px 10px',
+    width: '240px',
+});
+
 var InfoTip = React.createClass({
     getInitialState: function() {
         return {
@@ -8,14 +84,15 @@ var InfoTip = React.createClass({
     },
 
     render: function() {
-        return <span className="info-tip">
-            <i className="icon-question-sign"
+        return <span className={infoTip}>
+            <i className={`icon-question-sign ${infoTipI}`}
                onMouseEnter={this.handleMouseEnter}
                onMouseLeave={this.handleMouseLeave} />
-            <span className="info-tip-container"
+            <span className={infoTipContainer}
                   style={{display: this.state.hover ? 'block' : 'none'}}>
-                <span className="info-tip-triangle" />
-                <span className="info-tip-content-container vertical-shadow">
+                <span className={infoTipTriangle} />
+                {/* keep the classes here - used for selectors on KA */}
+                <span className={infoTipContentContainer}>
                     {this.props.children}
                 </span>
             </span>
--- js/tooltip.jsx
+++ js/tooltip.jsx
@@ -38,127 +38,124 @@
 //    `-''`''
 // Here be dragons.
 
-var TooltipArrow = (() => {
+var Triangle = React.createClass({
+    propTypes: {
+        color: React.PropTypes.string.isRequired,
+        left: React.PropTypes.number.isRequired,
+        "top": React.PropTypes.number.isRequired,
+        width: React.PropTypes.number.isRequired,
+        height: React.PropTypes.number.isRequired,
+        horizontalDirection: React.PropTypes.oneOf(
+            ["left", "right"]
+        ).isRequired,
+        verticalDirection: React.PropTypes.oneOf(
+            ["top", "bottom"]
+        ).isRequired,
+    },
 
-    // Scoped to only be used by TooltipArrow
-    var Triangle = React.createClass({
-        propTypes: {
-            color: React.PropTypes.string.isRequired,
-            left: React.PropTypes.number.isRequired,
-            "top": React.PropTypes.number.isRequired,
-            width: React.PropTypes.number.isRequired,
-            height: React.PropTypes.number.isRequired,
-            horizontalDirection: React.PropTypes.oneOf(
-                ["left", "right"]
-            ).isRequired,
-            verticalDirection: React.PropTypes.oneOf(
-                ["top", "bottom"]
-            ).isRequired,
-        },
+    render: function() {
+        var borderLeft, borderRight, borderTop, borderBottom;
 
-        render: function() {
-            var borderLeft, borderRight, borderTop, borderBottom;
-            if (this.props.horizontalDirection === "right") {
-                borderLeft = this.props.width + "px solid transparent";
-            } else {
-                borderRight = this.props.width + "px solid transparent";
-            }
-            if (this.props.verticalDirection === "top") {
-                borderTop = this.props.height + "px solid " +
-                        this.props.color;
-            } else {
-                borderBottom = this.props.height + "px solid " +
-                        this.props.color;
-            }
+        var hBorder = `${this.props.width}px solid transparent`;
+        if (this.props.horizontalDirection === "right") {
+            borderLeft = hBorder;
+        } else {
+            borderRight = hBorder;
+        }
 
-            return <div style={{
+        var vBorder = `${this.props.height}px solid ${this.props.color}`;
+        if (this.props.verticalDirection === "top") {
+            borderTop = vBorder;
+        } else {
+            borderBottom = vBorder;
+        }
+
+        return <div style={{
+            display: "block",
+            height: 0,
+            width: 0,
+            position: "absolute",
+            left: this.props.left,
+            "top": this.props["top"],
+            borderLeft: borderLeft,
+            borderRight: borderRight,
+            borderTop: borderTop,
+            borderBottom: borderBottom
+        }} />;
+    }
+});
+
+var TooltipArrow = React.createClass({
+    propTypes: {
+        position: React.PropTypes.string,
+        visibility: React.PropTypes.string,
+        left: React.PropTypes.number,
+        "top": React.PropTypes.number,
+        color: React.PropTypes.string.isRequired,  // a css color
+        border: React.PropTypes.string.isRequired,  // a css color
+        width: React.PropTypes.number.isRequired,
+        height: React.PropTypes.number.isRequired,
+        horizontalDirection: React.PropTypes.oneOf(
+            ["left", "right"]
+        ).isRequired,
+        verticalDirection: React.PropTypes.oneOf(
+            ["top", "bottom"]
+        ).isRequired
+    },
+
+    getDefaultProps: function() {
+        return {
+            position: "relative",
+            visibility: "visible",
+            left: 0,
+            "top": 0
+        };
+    },
+
+    // TODO(jack): Think about adding a box-shadow to the triangle here
+    // See http://css-tricks.com/triangle-with-shadow/
+    render: function() {
+        var isRight = (this.props.horizontalDirection === "right");
+        var isTop = (this.props.verticalDirection === "top");
+
+        var frontTopOffset = isTop ? 0 : 1;
+        var borderTopOffset = isTop ? 0 : -1;
+
+        return <div style={{
                 display: "block",
-                height: 0,
-                width: 0,
-                position: "absolute",
+                position: this.props.position,
+                visibility: this.props.visibility,
                 left: this.props.left,
                 "top": this.props["top"],
-                borderLeft: borderLeft,
-                borderRight: borderRight,
-                borderTop: borderTop,
-                borderBottom: borderBottom
-            }} />;
-        }
-    });
-
-    return React.createClass({
-        propTypes: {
-            position: React.PropTypes.string,
-            visibility: React.PropTypes.string,
-            left: React.PropTypes.number,
-            "top": React.PropTypes.number,
-            color: React.PropTypes.string.isRequired,  // a css color
-            border: React.PropTypes.string.isRequired,  // a css color
-            width: React.PropTypes.number.isRequired,
-            height: React.PropTypes.number.isRequired,
-            horizontalDirection: React.PropTypes.oneOf(
-                ["left", "right"]
-            ).isRequired,
-            verticalDirection: React.PropTypes.oneOf(
-                ["top", "bottom"]
-            ).isRequired
-        },
-
-        getDefaultProps: function() {
-            return {
-                position: "relative",
-                visibility: "visible",
-                left: 0,
-                "top": 0
-            };
-        },
-
-        // TODO(jack): Think about adding a box-shadow to the triangle here
-        // See http://css-tricks.com/triangle-with-shadow/
-        render: function() {
-            var isRight = (this.props.horizontalDirection === "right");
-            var isTop = (this.props.verticalDirection === "top");
-
-            var frontTopOffset = isTop ? 0 : 1;
-            var borderTopOffset = isTop ? 0 : -1;
-
-            return <div style={{
-                    display: "block",
-                    position: this.props.position,
-                    visibility: this.props.visibility,
-                    left: this.props.left,
-                    "top": this.props["top"],
-                    width: this.props.width + 2,
-                    height: this.props.height + 1,
-                    marginTop: -1,
-                    marginBottom: -2,
-                    zIndex: 1
-                }}>
-                {/* The background triangle used to create the effect of a
-                    border around the foreground triangle*/}
-                <Triangle
-                    horizontalDirection={this.props.horizontalDirection}
-                    verticalDirection={this.props.verticalDirection}
-                    color={this.props.border}
-                    left={0}
-                    top={borderTopOffset}
-                    width={this.props.width + 2}  // one extra for the diagonal
-                    height={this.props.height + 2} />
-                {/* The foreground triangle covers all but the left/right edges
-                    of the background triangle */}
-                <Triangle
-                    horizontalDirection={this.props.horizontalDirection}
-                    verticalDirection={this.props.verticalDirection}
-                    color={this.props.color}
-                    left={1}
-                    top={frontTopOffset}
-                    width={this.props.width}
-                    height={this.props.height} />
-            </div>;
-        }
-    });
-})();
-
+                width: this.props.width + 2,
+                height: this.props.height + 1,
+                marginTop: -1,
+                marginBottom: -2,
+                zIndex: 1
+            }}>
+            {/* The background triangle used to create the effect of a
+                border around the foreground triangle*/}
+            <Triangle
+                horizontalDirection={this.props.horizontalDirection}
+                verticalDirection={this.props.verticalDirection}
+                color={this.props.border}
+                left={0}
+                top={borderTopOffset}
+                width={this.props.width + 2}  // one extra for the diagonal
+                height={this.props.height + 2} />
+            {/* The foreground triangle covers all but the left/right edges
+                of the background triangle */}
+            <Triangle
+                horizontalDirection={this.props.horizontalDirection}
+                verticalDirection={this.props.verticalDirection}
+                color={this.props.color}
+                left={1}
+                top={frontTopOffset}
+                width={this.props.width}
+                height={this.props.height} />
+        </div>;
+    }
+});
 
 var VERTICAL_CORNERS = {
     "top": {
