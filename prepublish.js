61754cd89eab8634c9f942067c5f91c827599580
Joel Burget <joel@khanacademy.org>
2014-05-28 17:32:05 -0700

    tmp

--- prepublish.js
+++ prepublish.js
@@ -1,9 +1,19 @@
 'use strict';
 
 var fs = require('fs');
+var visitors = require('react-tools/vendor/fbtransform/visitors');
+var jstransformer = require('jstransform');
 
+var visitorList = visitors.getAllVisitors();
+
+// perform es6 / jsx tranforms on all files and simultaneously copy them to the
+// top level.
 var files = fs.readdirSync('js');
 for (var i = 0; i < files.length; i++) {
-    var file = files[i];
-    fs.renameSync('js/' + file, file);
+    var filename = files[i];
+
+    var js = fs.readFileSync('js/' + filename);
+    var transformed = jstransform.transform(visitorList, js).code;
+
+    fs.writeFileSync(filename, transformed);
 }
