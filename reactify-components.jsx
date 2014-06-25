b8669de504db84b9f020a718cbf8fc34560d7f99
Joel Burget <joel@khanacademy.org>
2014-06-25 10:49:27 -0700

    Add `<DragTarget>`.
    This previously existed in both webapp and perseus. And the two versions
    had diverged!
    
    Auditors: kevin

--- reactify-components.jsx
+++ reactify-components.jsx
@@ -7,17 +7,18 @@ var ReactPlayground = require('./react-live-editor/live-editor.jsx');
 window.Backbone = require('backbone');
 
 // Bring all these into scope
-window.BackboneMixin = require("./js/backbone-mixin.jsx");
-window.BlurInput = require("./js/blur-input.jsx");
-window.ButtonGroup = require("./js/button-group.jsx");
-window.$_ = require("./js/i18n.jsx");
-window.InfoTip = require("./js/info-tip.jsx");
+window.$_                    = require("./js/i18n.jsx");
+window.BackboneMixin         = require("./js/backbone-mixin.jsx");
+window.BlurInput             = require("./js/blur-input.jsx");
+window.ButtonGroup           = require("./js/button-group.jsx");
+window.DragTarget            = require("./js/drag-target.jsx");
+window.InfoTip               = require("./js/info-tip.jsx");
 window.LayeredComponentMixin = require("./js/layered-component-mixin.jsx");
-window.SetIntervalMixin = require("./js/set-interval-mixin.jsx");
-window.Sortable = require("./js/sortable.jsx");
-window.TeX = require("./js/tex.jsx");
-window.TimeAgo = require("./js/timeago.jsx");
-window.Tooltip = require("./js/tooltip.jsx");
+window.SetIntervalMixin      = require("./js/set-interval-mixin.jsx");
+window.Sortable              = require("./js/sortable.jsx");
+window.TeX                   = require("./js/tex.jsx");
+window.TimeAgo               = require("./js/timeago.jsx");
+window.Tooltip               = require("./js/tooltip.jsx");
 
 var examples = document.querySelectorAll('div.example_div');
 
