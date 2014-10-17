4df49cf8d71225a8c6b023e4e063fbee0146bbaf
Ben Alpert <ben@benalpert.com>
2014-10-17 14:47:59 -0700

    Fix i18n tests
    We need to use something like https://github.com/spicyj/jsx-i18n to make this not compile into a React.createElement call, but we're just testing the logic here so I'm not going to use JSX.
    
    Test Plan: npm test
    
    Auditors: joel

--- test/i18n-test.jsx
+++ test/i18n-test.jsx
@@ -9,15 +9,16 @@ var maybeJoin = function(renderable) {
 
 describe("i18n", function() {
     it("returns an array of just the string when no args", function() {
-        assert.deepEqual(maybeJoin(<$_>Khan Academy</$_>), "Khan Academy");
+        assert.deepEqual(maybeJoin($_(null, "Khan Academy")), "Khan Academy");
     });
 
     it("returns an array representing the string", function() {
         assert.deepEqual(
             maybeJoin(
-                <$_ who="anyone anywhere">
-                  A free world-class education for %(who)s.
-                </$_>
+                $_(
+                    {who: "anyone anywhere"},
+                    "A free world-class education for %(who)s."
+                )
             ),
             "A free world-class education for anyone anywhere."
         );
