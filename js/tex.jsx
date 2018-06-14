Khan/react-components

commit 9585917e02e7dfa3443a0d31ee7a3d2da54af99a

Do not use describedByIdCounter for initial render.
Summary:
The initial render of a component needs to match what was rendered on
the server. describedByIdCounter is reused between multiple SSR renders,
so will not match what is on the client (except for the first time the
SSR server renders something.)

Test Plan:
 - Went to http://localhost:8081/gandalf and enabled react-ssr.
 - Loaded http://localhost:8081/math/ap-calculus-ab/ab-antiderivatives-ftc/ab-u-substitution/a/u-substitution-definite-integrals with SSR
 - Did not get any popups telling me that the markup did not match.

Reviewers: davidflanagan, emily

Reviewed By: davidflanagan, emily

Spies: russell, tom

Differential Revision: https://phabricator.khanacademy.org/D43732

js/tex.jsx
@@ -95,6 +95,15 @@ const TeX = React.createClass({
 
     mixins: [PureRenderMixin],
 
+    // TODO(joshuan): Once we are using React 16.3+,
+    // migrate to getDerivedStateFromProps
+    getInitialState: function() {
+        return {
+            mounted: false,
+            katexHtml: this.getKatexHtml(this.props),
+        };
+    },
+
     getDefaultProps: function() {
         return {
             katexOptions: {
@@ -115,6 +124,12 @@ const TeX = React.createClass({
     componentDidMount: function() {
         this._root = ReactDOM.findDOMNode(this);
 
+        // Needed so that the initial client render matches SSR.
+        // eslint-disable-next-line react/no-did-mount-set-state
+        this.setState({
+            mounted: true,
+        });
+
         if (this.refs.katex.childElementCount > 0) {
             // If we already rendered katex in the render function, we don't
             // need to render anything here.
@@ -126,6 +141,41 @@ const TeX = React.createClass({
 
         this.setScriptText(text);
         this.process(() => this.props.onRender(this._root));
+    },
+
+    // TODO(joshuan): If you are updating to React 16.3+, migrate to
+    // getDerivedStateFromProps
+    componentWillReceiveProps: function(nextProps) {
+        if (
+            nextProps.children !== this.props.children ||
+            JSON.stringify(nextProps.katexOptions) !==
+                JSON.stringify(this.props.katexOptions)
+        ) {
+            this.setState({
+                katexHtml: this.getKatexHtml(nextProps),
+            });
+        }
+    },
+
+    getKatexHtml(props) {
+        // Try to render the math content with KaTeX.
+        // If this fails, componentDidUpdate() will notice and
+        // use MathJAX instead.
+        try {
+            return {
+                __html: katex.renderToString(
+                    props.children,
+                    props.katexOptions,
+                ),
+            };
+        } catch (e) {
+            /* jshint -W103 */
+            if (e.__proto__ !== katex.ParseError.prototype) {
+                /* jshint +W103 */
+                throw e;
+            }
+            return null;
+        }
     },
 
     componentDidUpdate: function(prevProps, prevState) {
@@ -216,25 +266,7 @@ const TeX = React.createClass({
     },
 
     render: function() {
-        // First, try to render the math content with KaTeX.
-        // If this fails, we ignore the KaTeX error and just render
-        // an empty span. Later, componentDidUpdate() will notice
-        // and will use MathJAX instead.
-        let katexHtml = null;
-        try {
-            katexHtml = {
-                __html: katex.renderToString(
-                    this.props.children,
-                    this.props.katexOptions,
-                ),
-            };
-        } catch (e) {
-            /* jshint -W103 */
-            if (e.__proto__ !== katex.ParseError.prototype) {
-                /* jshint +W103 */
-                throw e;
-            }
-        }
+        const {katexHtml} = this.state;
 
         // If we successfully parsed with KaTeX, then try parse the
         // same math text to an english rendering that can be read
@@ -248,7 +280,12 @@ const TeX = React.createClass({
                 katexA11yHtml = {
                     __html: katexA11y.renderString(this.props.children),
                 };
-                describedById = `katex-${++describedByIdCounter}`;
+                // The server and the client will not necessarily use the same
+                // ID, and the initial client markup must match the server
+                // markup. Thus, we only add an ID once we've rendered once.
+                if (this.state.mounted) {
+                    describedById = `katex-${++describedByIdCounter}`;
+                }
             } catch (e) {
                 // Nothing
             }

commit 201ff43698bbee4f432d721b58a27f901dc6f584

Do not needlessly update MathJax
9585917e02e7dfa3443a0d31ee7a3d2da54af99a introduced a regression where
the TeX component needlessly updated MathJax when the source didn't
change. This caused the "TeX MathJax waits for MathJax to load before
rendering" test to fail, which expected MathJax to be requested exactly
once.

Test Plan:
 - npm run test
 - In Khan Academy's webapp, went to http://localhost:8081/devadmin/content/exercises/estimating-with-multiplying-decimals/xa54e096e/x6f72a844130a4843 and pasted
$2.7 \times 42 \approx %$, which cannot be rendered via KaTeX due to the comment
 - Saw that the expression rendered.
 - Changed the expression while still retaining the comment
 - Saw that the expression updated

Auditors: davidflanagan, emily

js/tex.jsx
@@ -179,43 +179,45 @@ const TeX = React.createClass({
     },
 
     componentDidUpdate: function(prevProps, prevState) {
-        this.maybeUnprocess();
+        if (this.props.children !== prevProps.children) {
+            this.maybeUnprocess();
 
-        // If we already rendered katex in the render function, we don't
-        // need to render anything here.
-        if (this.refs.katex.childElementCount > 0) {
-            if (this.script) {
-                // If we successfully rendered KaTeX, check if there's
-                // lingering MathJax from the last render, and if so remove it.
-                loadMathJax(() => {
-                    const jax = MathJax.Hub.getJaxFor(this.script);
-                    if (jax) {
-                        jax.Remove();
-                    }
-                });
+            // If we already rendered katex in the render function, we don't
+            // need to render anything here.
+            if (this.refs.katex.childElementCount > 0) {
+                if (this.script) {
+                    // If we successfully rendered KaTeX, check if there's
+                    // lingering MathJax from the last render, and if so remove it.
+                    loadMathJax(() => {
+                        const jax = MathJax.Hub.getJaxFor(this.script);
+                        if (jax) {
+                            jax.Remove();
+                        }
+                    });
+                }
+
+                this.props.onRender();
+                return;
             }
 
-            this.props.onRender();
-            return;
-        }
+            const newText = this.props.children;
 
-        const newText = this.props.children;
-
-        if (this.script) {
-            loadMathJax(() => {
-                MathJax.Hub.Queue(() => {
-                    const jax = MathJax.Hub.getJaxFor(this.script);
-                    if (jax) {
-                        return jax.Text(newText, this.props.onRender);
-                    } else {
-                        this.setScriptText(newText);
-                        this.process(this.props.onRender);
-                    }
+            if (this.script) {
+                loadMathJax(() => {
+                    MathJax.Hub.Queue(() => {
+                        const jax = MathJax.Hub.getJaxFor(this.script);
+                        if (jax) {
+                            return jax.Text(newText, this.props.onRender);
+                        } else {
+                            this.setScriptText(newText);
+                            this.process(this.props.onRender);
+                        }
+                    });
                 });
-            });
-        } else {
-            this.setScriptText(newText);
-            this.process(this.props.onRender);
+            } else {
+                this.setScriptText(newText);
+                this.process(this.props.onRender);
+            }
         }
     },
 